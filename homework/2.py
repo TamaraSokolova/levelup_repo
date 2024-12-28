721b2086265c8a6fdf70d7ce0d230b285e26eec8
Tomasina-777 <mara-777@bk.ru>
2024-12-28 16:21:00 +0300

    level1

---
+++ homework/2.py
@@ -0,0 +1,15 @@
+#1
+'''a=0
+for i in range(100):
+    a=a+i    
+print('a=', a)'''
+
+#2
+'''a=str(input('Введите слово: '))
+for i in a: #range(a,len(a)):
+    print(i) #, end='') '''
+
+#3
+x=int(input('Введите число: '))
+for i in range(1,11):
+    print(x*i)
