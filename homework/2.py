a10e78811d1ad9c54ee0aa08d95a372a127bbcfc
Tomasina-777 <mara-777@bk.ru>
2024-12-28 16:46:06 +0300

    level2_1

--- homework/2.py
+++ homework/2.py
@@ -2,7 +2,7 @@
 '''a=0
 for i in range(100):
     a=a+i    
-print('a=', a)'''
+print('Сумма=', a)'''
 
 #2
 '''a=str(input('Введите слово: '))
@@ -10,6 +10,22 @@ for i in a: #range(a,len(a)):
     print(i) #, end='') '''
 
 #3
-x=int(input('Введите число: '))
+'''x=int(input('Введите число: '))
 for i in range(1,11):
-    print(x*i)
+    print(x*i)'''
+
+#4
+'''a=0
+n=int(input('Введите число: '))
+for i in range(0,n+1, 2):   
+    a=a+i    
+print('Сумма четных чисел от 1 до ', n,' равна ', a)'''
+
+#5
+'''a=0
+n=int(input('Введите число: '))
+for i in range(n+1):   
+    while i%3==0:
+        a=a+i        
+        break
+print('Сумма чисел, кратных 3, от 1 до', n, 'равна', a) '''
