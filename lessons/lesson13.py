11363a68b1a2502a1d4adf63019af925d3fae287
Tomasina-777 <mara-777@bk.ru>
2025-02-03 20:32:10 +0300

    lessonsss

--- lessons/lesson13.py
+++ lessons/lesson13.py
@@ -7,33 +7,39 @@ class Animal():
         print(f'{self.name} - {self.species}')
 
 class Mammal(Animal):
-        def __init__(self, name, species, warm_blood):
+    def __init__(self, name, species, warm_blood):
             super().__init__(name, species)
             self.warm_blood = True
 
-        def display_info(self):
+    def display_info(self):
             print(f'warm_blood {self.name} - {self.species}, {self.warm_blood}')
 
 class Bird(Animal):
-        def __init__(self, name, species, can_fly):
+    def __init__(self, name, species, can_fly):
             super().__init__(name, species)
             self.can_fly = True
 
-        def display_info(self):
+    def display_info(self):
             print(f'can_fly {self.name} - {self.species}, {self.can_fly}')
 
-a = Animal(zebra, mammal, True)
-b = Bird(typic, bird, True)
+a = Animal('zebra', 'mammal', True)
+m = Mammal('m', 'm', True)
+m.display_info()
+b = Bird('typic', 'bird', True)
+b.display_info()
     
-'''class Zoo():
+class Zoo():
     def __init__(self, animals):
-        self.animals = animals
+        self.animals = []
 
     def add_animal(self, animals):
         print(animals)
+        for i in animals:
+            self.i.append(animals)
     
     def show_all_animals(self):
         print ('животные')
         for i in animals:
-            self.i.append(animals)'''
+             print(i)
+            
 
